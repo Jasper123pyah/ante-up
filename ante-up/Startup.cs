f5e6e09c6af7695cd137b5d9442e57e011a3b2f3
Jasper123pyah <73039915+Jasper123pyah@users.noreply.github.com>
2021-09-07 22:37:20 +0200

    api connection test

--- ante-up/Startup.cs
+++ ante-up/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -16,6 +17,7 @@ namespace ante_up
 {
     public class Startup
     {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,6 +28,15 @@ namespace ante_up
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                    builder =>
+                    {
+                        builder.WithOrigins("185.82.192.67:3000","localhost:3000").AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
             services.AddControllers();
         }
 
@@ -40,6 +51,8 @@ namespace ante_up
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseAuthorization();
             app.UseCors(builder =>
@@ -49,7 +62,14 @@ namespace ante_up
                     .WithOrigins("http://localhost:3000/")
                     .AllowCredentials();
             });
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/echo",
+                    context => context.Response.WriteAsync("echo"))
+                    .RequireCors(MyAllowSpecificOrigins);
+                endpoints.MapControllers().RequireCors(MyAllowSpecificOrigins); 
+                
+            });
         }
     }
 }
