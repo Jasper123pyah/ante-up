9af22f14be0e3490018f7b24c200f911d59b341a
Jasper123pyah <73039915+Jasper123pyah@users.noreply.github.com>
2021-09-07 17:18:50 +0200

    Extra files

---
+++ ante-up/Controllers/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ante_up.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ante_up.Controllers
+{
+    namespace Together.Controllers
+    {
+        [ApiController]
+        [Route("[controller]")]
+        public class GameController : ControllerBase
+        {
+            [HttpGet]
+            public List<string> GetGames()
+            {
+                List<string> list = new()
+                {
+                    "1",
+                    "2",
+                    "3"
+                };
+                return list;
+            }
+
+            [HttpPost("/add")]
+            public Game NewGame(Game newGame)
+            {
+                Console.WriteLine(newGame);
+                Game game = new Game()
+                {
+                    Name = newGame.Name
+                };
+                // data.addnewgame
+                Console.WriteLine("Game Added");
+                return null;
+            }
+        }
+    }
+}
