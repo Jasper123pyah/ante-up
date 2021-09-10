8270843caac8c5861b58cd10f52dadd3962d8715
Jasper123pyah <73039915+Jasper123pyah@users.noreply.github.com>
2021-09-10 22:12:56 +0200

    extra commit

---
+++ ante-up.Common/ApiModels/ApiAccountInfo.cs
@@ -0,0 +1,9 @@
+namespace ante_up.Common.ApiModels
+{
+    public class ApiAccountInfo
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public int Balance { get; set; }
+    }
+}
---
+++ ante-up.Common/Global.cs
@@ -0,0 +1,7 @@
+namespace ante_up.Common
+{
+    public static class Global
+    {
+        public static string Secret { get; } = "872031924926476fb05eeddd8d366b44";
+    }
+}
